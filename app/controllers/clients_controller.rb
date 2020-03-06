e6050f3dfab601de312483a6bbd731828590c020
github username <github email address>
2020-03-06 11:39:26 -0500

    added post route and login form in clients login.erb

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -9,7 +9,7 @@ class ClientsController < ApplicationController
     end
 
     post '/signup' do
-        @client = Client.new(name: params[:name], phone_num: params[:phone_num], email: params[:email], password: params[:password])
+    @client = Client.new(name: params[:name], phone_num: params[:phone_num], email: params[:email], password: params[:password])
         if @client.save && @client.name != "" && @client.email != "" && @client.authenticate(params[:password])
             session[:client_id] = @client.id
             redirect '/services'
@@ -24,7 +24,18 @@ class ClientsController < ApplicationController
         else
         erb :'/clients/login'
         end
-  end
+    end
+
+    post '/login' do
+    client = client.find_by(name: params[:name])
+        if client && client.authenticate(params[:password])
+        session[:client_id] = client.id
+        redirect '/services'
+        else
+        redirect '/login'
+        end
+
+    end
 
 
 end
