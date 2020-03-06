509ecd010b876a4e5fdf085684ffa4b3508e3e15
github username <github email address>
2020-03-06 00:06:22 -0500

    added services post route

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -14,7 +14,16 @@ class ServicesController < ApplicationController
             redirect '/login'
         end
         erb :'services/new'
+    end
 
+    post '/services' do
+        #  binding.pry
+        client = current_client
+        # if params[:service_name].empty?
+        #     redirect '/services/new' 
+        # end  
+        @service = Service.create(service_name: params[:service_name], :client_id => client.id)
+        redirect '/services'
     end
 
 end
