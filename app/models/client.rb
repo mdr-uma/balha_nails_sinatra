6c2c8354555a0d54818fe95ef764fc006bd0ef38
github username <github email address>
2020-03-07 13:56:27 -0500

    added image to welcome page

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,7 @@ class Client < ActiveRecord::Base
     has_secure_password
     has_many :appointments
     validates :email, uniqueness: true
+    
+    # validates :legacy_code, format: {with: /\A[a-zA-Z]+\z/, 
+    # message: "only allows letters"}
 end
