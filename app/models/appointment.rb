3f8fcb7642d24b73028c3c447bd5cee266812f4b
github username <github email address>
2020-03-09 12:52:27 -0400

    added date validation to new and edit form

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,3 +1,11 @@
 class Appointment < ActiveRecord::Base
     belongs_to :client
+
+    # def date
+    #  if date >= Date.current
+    #     date
+    #  else
+    #     invalid date
+    #  end
+    # end
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -3,6 +3,5 @@ class Client < ActiveRecord::Base
     has_many :appointments
     validates :email, uniqueness: true
     
-    # validates :legacy_code, format: {with: /\A[a-zA-Z]+\z/, 
-    # message: "only allows letters"}
+    # validates :name, format: {with: /\A[a-zA-Z]+\z/, message: "only allows letters"}
 end
